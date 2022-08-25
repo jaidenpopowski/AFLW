e711444c0017159bf6565f42776880d2e8b37f4e
Jaiden Popowski <98011675+jaidenpopowski@users.noreply.github.com>
2022-08-25 13:53:02 +1000

    Add files via upload

---
+++ AFLW Script.R
@@ -0,0 +1,34 @@
+# AFLW Historical Statistics
+
+# Package names
+packages <- c("tidyverse", "fitzRoy")
+
+# Install packages not yet installed
+installed_packages <- packages %in% rownames(installed.packages())
+if (any(installed_packages == FALSE)) {
+  install.packages(packages[!installed_packages])
+}
+
+# Packages loading
+invisible(lapply(packages, library, character.only = TRUE))
+
+# Read in data from 2017-2022A seasons
+afl_w_history <- read_csv("AFLW History.csv")
+
+
+# calculate team totals across stat lines
+game_totals <- afl_w_history %>% 
+  select(-lastUpdated) %>% 
+  group_by(providerId,team.name) %>% 
+  summarise(across(c(goals:`clearances.totalClearances`), sum),.groups = 'drop') %>% 
+  select(-ratingPoints,-ranking,-superGoals,-goalEfficiency,-shotEfficiency,-interchangeCounts)
+
+colnames(game_totals) <- paste("team",colnames(game_totals),sep=".")
+colnames(game_totals)[1] <- 'providerId'
+colnames(game_totals)[2] <- 'team.name'
+
+# collate final data
+data <- afl_w_history %>% 
+  left_join(game_totals, by = c("providerId","team.name"))
+
+write.csv(data, file = "AFLW Statistics.csv")
